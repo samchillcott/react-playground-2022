5ec9f3719333e0f5500de6c576f8d2907519a3e0
samchillcott1 <samchillcott@gmail.com>
2022-11-12 08:58:05 +0000

    add basic form structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const defaultFormData = {
+  title: "",
+  body: "",
+};
+
+export default function App() {
+  const [formData, setFormData] = useState(defaultFormData);
+  const { title, body } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData(defaultFormData);
+  };
+
   return (
-    <div className="App">
-test
-    </div>
+    <>
+      <h1>Form</h1>
+      <p>Create a post</p>
+
+      <form onSubmit={ onSubmit }>
+        <label htmlFor="title">Title</label>
+        <br />
+        <input type="text" id="title" value={ title } onChange={ onChange } />
+        <br />
+        <br />
+        <label htmlFor="body">Body</label>
+        <br />
+        <input type="text" id="body" value={ body } onChange={ onChange } />
+        <br />
+        <br />
+        <button type="submit">Upload post</button>
+      </form>
+    </>
   );
 }
-
-export default App;
