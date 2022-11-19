72fb40eeb0e8dd23c608f1792bea8dbd7461665c
samchillcott1 <samchillcott@gmail.com>
2022-11-19 11:40:14 +0000

    setup custom hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import ComponentA from './ComponentA'
+import ComponentB from './ComponentB'
 
 const App = () => {
   return (
-    <ComponentA />
-    <ComponentB />
+    <>
+      <ComponentA />
+      <ComponentB />
+    </>
   )
 }
 
--- src/ComponentA.js
+++ src/ComponentA.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import useNumber from './hooks/useNumber'
 
 const ComponentA = () => {
+ const number = useNumber()
+ console.log(number);
   return (
     <div>ComponentA</div>
   )
