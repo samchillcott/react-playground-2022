277af024ec4c81af5c5d265b4e16eae476cbac68
samchillcott1 <samchillcott@gmail.com>
2022-11-19 11:34:52 +0000

    custom hooks setup

--- src/App.js
+++ src/App.js
@@ -1,59 +1,10 @@
-import axios from "axios";
-import { useState } from "react";
+import React from 'react'
 
-const defaultFormData = {
-  title: "",
-  body: "",
-  userId: 1,
-};
-
-export default function App() {
-  const [formData, setFormData] = useState(defaultFormData);
-  const [success, setSuccess] = useState(false)
-  const [error, setError] = useState(false)
-  const { title, body } = formData;
-
-  const onChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-  };
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post("https://jsonplaceholder.typicode.com/posts/", formData)
-      setSuccess(true)
-      setError(false)
-    } catch (error) {
-      setSuccess(false)
-      setError(true)
-    }
-    setFormData(defaultFormData);
-  };
-
+const App = () => {
   return (
-    <>
-      <h1>Form</h1>
-      <p>Create a post</p>
-
-      <form onSubmit={ onSubmit }>
-        <label htmlFor="title">Title</label>
-        <br />
-        <input type="text" id="title" value={ title } onChange={ onChange } />
-        <br />
-        <br />
-        <label htmlFor="body">Body</label>
-        <br />
-        <input type="text" id="body" value={ body } onChange={ onChange } />
-        <br />
-        <br />
-        <button type="submit">Upload post</button>
-      </form>
-      { error && <p>Oops, could not upload post</p> }
-      { success && <p>Post upload has succeeded</p> }
-    </>
-
-  );
+    <ComponentA />
+    <ComponentB />
+  )
 }
+
+export default App
