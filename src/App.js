6234ce288b3b0410942ca18db7b271981e55d5f8
samchillcott1 <samchillcott@gmail.com>
2022-12-20 10:59:37 +0000

    setup client provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { QueryClientProvider, QueryClient } from 'react-query'
 
 const App = () => {
+
+  const queryClient = new QueryClient()
   return (
     <>
- 
+      <QueryClientProvider client={queryClient}>
+
+      </QueryClientProvider>
     </>
   )
 }
