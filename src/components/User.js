a076b73fadf1557faaffeba5420a5db02b808a73
samchillcott1 <samchillcott@gmail.com>
2022-12-21 11:23:08 +0000

    add loading state

--- src/components/User.js
+++ src/components/User.js
@@ -4,7 +4,11 @@ import useUser from '../query hooks/useUser'
 const User = () => {
   const user = useUser(5)
   return (
-    user.isSuccess && <p>{`${user.data.name} - ${user.data.address.street}`}</p>
+    <>
+      { user.isLoading && <p>Loading data...</p> }
+      { user.isSuccess && <p>{ `${user.data.name} - ${user.data.address.street}` }</p>
+      }
+    </>
   )
 }
 
