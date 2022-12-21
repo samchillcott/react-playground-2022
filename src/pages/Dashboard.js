0a8c5f640ed45fa49bc3472f71e603c4390960b2
samchillcott1 <samchillcott@gmail.com>
2022-12-21 11:59:49 +0000

    add loading state

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,14 +1,17 @@
 import { React, useState } from 'react'
 import User from '../components/User'
 import Users from '../components/Users'
+import { useIsFetching } from "react-query";
 
 const Dashboard = () => {
   const [selectedUser, setSelectedUser] = useState(null)
+  const isFetching = useIsFetching()
   return (
     <>
       <Users setSelectedUser={ setSelectedUser } />
       <hr />
       <User selectedUser={ selectedUser }/>
+      { isFetching ? <p>Loading...</p> : null}
     </>
   )
 }
